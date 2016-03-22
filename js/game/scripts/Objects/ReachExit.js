df000190b151a0af9c15719224b8b1b1a0f937a7
Renan Oliveira Netto <renan.o.netto@gmail.com>
2016-03-21 22:14:42 -0300

    Player stats being saved when reaching exit

--- js/game/scripts/Objects/ReachExit.js
+++ js/game/scripts/Objects/ReachExit.js
@@ -13,6 +13,14 @@ DungeonExplorer.ReachExit.prototype.constructor = DungeonExplorer.ReachExit;
 DungeonExplorer.ReachExit.prototype.reach_exit = function () {
     "use strict";
     if (!this.locked) {
+        this.save_persistent_data();
         this.game_state.game.state.start("BootState", true, false, "DungeonState", "assets/levels/dungeon.json", {level: this.game_state.current_level + 1});
     }
+};
+
+DungeonExplorer.ReachExit.prototype.save_persistent_data = function () {
+    "use strict";
+    this.game_state.persistent_data.player = {
+        "player_stats.stats": this.game_state.prefabs.player.scripts.player_stats.stats
+    };
 };
